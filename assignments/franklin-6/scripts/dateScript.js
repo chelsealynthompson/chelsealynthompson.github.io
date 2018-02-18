06a7c4be2c279c4d4922c7287d888388f7221a81
chelsea <chelseawaite@Chelseas-MacBook-Pro.local>
2018-02-17 16:35:37 -0800

    week6

---
+++ assignments/franklin-6/scripts/dateScript.js
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+currentdate () {
+var theMonths = ["January", "February", "March", 
+"April", "May", "June", "July", "August", "September", 
+"October", "November", "December"];
+
+var theDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var d = new Date();
+var dayName = theDays[d.getDay()];
+var day = d.getDate();
+var monthName = theMonths[d.getMonth()];
+var year = d.getFullYear();
+
+document.getElementById("currentdate").innerHTML = dayName + ", " + day + " " monthName + " " + year;
+}
+</script>
