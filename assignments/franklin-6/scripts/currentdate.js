3783c07984696d0167b24929bc783aa3bb4c89ef
chelsea <chelseawaite@Chelseas-MacBook-Pro.local>
2018-02-19 14:30:48 -0800

    week 6

--- assignments/franklin-6/scripts/currentdate.js
+++ assignments/franklin-6/scripts/currentdate.js
@@ -1,13 +1,11 @@
-var theMonths = ["January", "February", "March", 
-"April", "May", "June", "July", "August", "September", 
-"October", "November", "December"];
-
 var theDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var theMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 
 var d = new Date();
-var dayName = theDays[d.getDay()];
+var dayName = theDay[d.getDay()];
 var day = d.getDate();
 var monthName = theMonths[d.getMonth()];
 var year = d.getFullYear();
 
-document.getElementById("currentdate").innerHTML = dayName + ", " + day + " " monthName + " " + year;
+document.getElementById("currentdate").innerHTML = dayName + ", " + day + " " + monthName + " " + year;
